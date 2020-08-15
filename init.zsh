058ee1b2485feb3300d5c5731c424b0f99957234
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-08-14 20:21:10 -0400

    Initial Commit

---
+++ init.zsh
@@ -0,0 +1,43 @@
+######################################################################
+#<
+#
+# Function: p6df::modules::db::version()
+#
+#>
+######################################################################
+p6df::modules::sqlite::version() { echo "0.0.1" }
+
+######################################################################
+#<
+#
+# Function: p6df::modules::db::external::brew()
+#
+#>
+######################################################################
+p6df::modules::sqlite::external::brew() {
+
+  brew install sqlite
+  brew install sqlite-analyzer
+  brew install sqlite-utils
+  brew install sqliteodbc
+
+  brew cask install db-browser-for-sqlite
+  brew cask install sqlitemanager
+  brew cask install sqlitestudio
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::db::home::symlink()
+#
+#>
+######################################################################
+p6df::modules::sqlite::home::symlink() {
+
+}
+
+p6df::modules::sqlite::init() {
+
+}
+
